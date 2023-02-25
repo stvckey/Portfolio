54e2801e6521276cfbfb8ab368db49c31e1d3ba4
BIGSTVCK <stvckey@LAPTOP-PIPJ2082.localdomain>
2023-02-24 23:36:23 -0500

    initial commit

---
+++ my-site/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+        fontFamily: {
+            teko: "Teko",
+            tekobold: "Teko-Bold",
+            firacode: "FiraCode",
+            firacode: "FiraCode-Bold",
+        },
+    },
+},
+  plugins: [],
+};
